cd33a5ec0ce0a4bc914b2f4b0df09111802e788e
Tomas Imaizumi <timaizumi@TIMAIZUMI-T14.localdomain>
2025-08-24 19:04:10 -0700

    Added unit test base class

---
+++ tests/TeamControllerTest.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include "domain/Team.hpp"
+
+
+TEST(TeamControllerTest, CreateTeam) {
+    std::string id = "ID";
+    std::string name = "Name";
+
+    domain::Team team = {id,name};    
+
+    EXPECT_EQ(team.Id.c_str(), id);
+    EXPECT_EQ(team.Name.c_str(), name);
+}
