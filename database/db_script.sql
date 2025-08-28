4d78bf48ce91dd6d16c728ef411cfc8b16eb619d
Tomas Imaizumi <timaizumi@TIMAIZUMI-T14.localdomain>
2025-08-28 16:09:01 -0700

    started db scripts

---
+++ database/db_script.sql
@@ -0,0 +1,20 @@
+CREATE USER tournament_svc WITH PASSWORD 'password';
+CREATE USER tournament_admin WITH PASSWORD 'password';
+
+CREATE DATABASE tournament_db;
+
+grant all privileges on database tournament_db to tournament_admin;
+grant all privileges on database tournament_db to tournament_svc;
+grant usage on schema public to tournament_admin;
+grant usage on schema public to tournament_svc;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO tournament_admin;
+GRANT DELETE ON ALL TABLES IN SCHEMA public TO tournament_admin;
+GRANT UPDATE ON ALL TABLES IN SCHEMA public TO tournament_admin;
+GRANT INSERT ON ALL TABLES IN SCHEMA public TO tournament_admin;
+GRANT CREATE ON SCHEMA public TO tournament_admin;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO tournament_svc;
+GRANT DELETE ON ALL TABLES IN SCHEMA public TO tournament_svc;
+GRANT UPDATE ON ALL TABLES IN SCHEMA public TO tournament_svc;
+GRANT INSERT ON ALL TABLES IN SCHEMA public TO tournament_svc;
